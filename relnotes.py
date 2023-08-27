0b5f8e4ad51d781e5293cac704e32cafd848db56
Alex Grönholm <alex.gronholm@nextday.fi>
2023-08-27 22:39:48 +0300

    Use the GITHUB_WORKSPACE directory to store the changelog

--- relnotes.py
+++ relnotes.py
@@ -57,7 +57,7 @@ def main() -> None:
 
     snippet = dedent("\n".join(lines)).strip()
     doc = pandoc.read(source=snippet, format=format_)
-    output_path = Path(os.environ["RUNNER_TEMP"]).joinpath("changelog.md")
+    output_path = Path(os.environ["GITHUB_WORKSPACE"]).joinpath("changelog.md")
     with output_path.open("w") as f:
         pandoc.write(doc, file=f, format="markdown")
 
