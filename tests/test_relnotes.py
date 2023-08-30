49133c84ff0ce4f0bbee33d11428608aefec408d
Alex Grönholm <alex.gronholm@nextday.fi>
2023-08-31 01:07:40 +0300

    Switched to Docker based tests

--- tests/test_relnotes.py
+++ tests/test_relnotes.py
@@ -1,14 +1,27 @@
 from __future__ import annotations
 
+from os import getuid
 from pathlib import Path
 from textwrap import dedent
 
-from _pytest.monkeypatch import MonkeyPatch
+import docker
+import pytest
 
-from relnotes import main
+
+@pytest.fixture(scope="session")
+def docker_client() -> docker.DockerClient:
+    return docker.from_env()
 
 
-def test_extract(tmp_path: Path, monkeypatch: MonkeyPatch) -> None:
+@pytest.fixture(scope="session")
+def docker_image(docker_client: docker.DockerClient) -> docker.Image:
+    image, logs = docker_client.images.build(path=str(Path(__file__).parent.parent))
+    return image
+
+
+def test_extract(
+    tmp_path: Path, docker_client: docker.DockerClient, docker_image: docker.Image
+) -> None:
     input_path = tmp_path / "changelog.rst"
     output_path = tmp_path / "output.txt"
     tmp_path / "changelog.md"
@@ -37,15 +50,19 @@ def test_extract(tmp_path: Path, monkeypatch: MonkeyPatch) -> None:
         )
     )
 
-    monkeypatch.setenv("GITHUB_WORKSPACE", str(tmp_path))
-    monkeypatch.setenv("GITHUB_REF_NAME", "1.2.3rc4")
-    monkeypatch.setenv("GITHUB_REF_TYPE", "tag")
-    monkeypatch.setenv("GITHUB_OUTPUT", str(output_path))
-    monkeypatch.setenv("INPUT_PATH", str(input_path))
-    monkeypatch.setenv("INPUT_HEADER", "Header\n======\n")
-    monkeypatch.setenv("INPUT_FOOTER", "Footer\n======\n")
-    monkeypatch.setenv("INPUT_VERSION_PATTERN", r"^\*\*([0-9][^*]*)\*\*")
-    main()
+    environment = {
+        "GITHUB_REF_NAME": "1.2.3rc4",
+        "GITHUB_REF_TYPE": "tag",
+        "GITHUB_OUTPUT": "/data/output.txt",
+        "INPUT_PATH": "/data/changelog.rst",
+        "INPUT_HEADER": "Header\n======\n",
+        "INPUT_FOOTER": "Footer\n======\n",
+        "INPUT_VERSION_PATTERN": r"^\*\*([0-9][^*]*)\*\*",
+    }
+    volumes = {str(tmp_path): {"bind": "/data", "mode": "rw"}}
+    docker_client.containers.run(
+        docker_image, environment=environment, user=getuid(), volumes=volumes
+    )
 
     assert output_path.read_text() == dedent(
         """\
